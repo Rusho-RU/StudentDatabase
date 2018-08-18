d764b9d1c45a857c415ce8dbd53c18e8afc854ea
Mohtasin Meherab Sikder <30768212+Rusho-RU@users.noreply.github.com>
2018-08-18 21:03:22 +0600

    Added layout and backend for inserting new data

--- app/src/main/java/com/dragontwister/studentdatabase/MainActivity.java
+++ app/src/main/java/com/dragontwister/studentdatabase/MainActivity.java
@@ -2,11 +2,59 @@ package com.dragontwister.studentdatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    StudentDBHandler dbHandler;
+    EditText editName, editSurname, editMarks;
+    Button btnAddData, btnReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dbHandler = new StudentDBHandler(this);
+
+        editName = findViewById(R.id.textName);
+        editSurname = findViewById(R.id.textSurname);
+        editMarks = findViewById(R.id.textMarks);
+        btnAddData = findViewById(R.id.buttonAdd);
+        btnReset = findViewById(R.id.buttonReset);
+        addData();
+        resetData();
+    }
+
+    public void resetData(){
+        btnReset.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        editName.getText().clear();
+                        editSurname.getText().clear();
+                        editMarks.getText().clear();
+                    }
+                }
+        );
+    }
+
+    public void addData(){
+        btnAddData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = dbHandler.inserData(editName.getText().toString(),
+                                editSurname.getText().toString(),
+                                editMarks.getText().toString());
+
+                        if(isInserted)
+                            Toast.makeText(MainActivity.this, "DATA INSERTED", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Something went wrong! Data was not inserted!", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/com/dragontwister/studentdatabase/StudentDBHandler.java
+++ app/src/main/java/com/dragontwister/studentdatabase/StudentDBHandler.java
@@ -1,5 +1,6 @@
 package com.dragontwister.studentdatabase;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -14,18 +15,30 @@ public class StudentDBHandler extends SQLiteOpenHelper {
 
     public StudentDBHandler(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + "(" + COL_1 + "INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + COL_2 + "TEXT," + COL_3 + "TEXT," + COL_4 + "INTEGER" + ")");
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(" + COL_1 + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COL_2 + " TEXT," + COL_3 + " TEXT," + COL_4 + " INTEGER" + ")");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean inserData(String name, String surname, String marks){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, surname);
+        contentValues.put(COL_4, marks);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        return result != -1;
     }
 }
